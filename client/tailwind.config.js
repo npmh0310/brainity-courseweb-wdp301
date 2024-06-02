fe00a87dfa178b36de21819793d5f7c40fe7b87c
imTaam0.0 <tamtvnde160577@fpt.edu.vn>
2024-06-03 02:12:08 +0700

    complete2

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -77,11 +77,9 @@ module.exports = {
         },
         'open': {
           '0%': {
-            transform: 'translateY(-20px)',
             opacity: 0,
           },
           '100%' : {
-            transform: 'translateY(0)',
             opacity: 1
           }
         }
