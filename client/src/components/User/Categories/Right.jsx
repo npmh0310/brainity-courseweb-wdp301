e1d85393aa04a2ec3aa33d3ec18287fd35ee3cd1
emnghiaproplayers <truongnghia297@gmail.com>
2024-07-23 08:31:33 +0700

    minor fix

--- client/src/components/User/Categories/Right.jsx
+++ client/src/components/User/Categories/Right.jsx
@@ -46,7 +46,7 @@ export const Right = ({ filteredCourses, currentPage,handlePageClick, totalPages
             {Array.from({ length: totalPages }, (_, index) => index + 1).map(
               (pageNumber) => (
                 // Phân trang vẫn còn lỗi khi filter nên tạm thời set totalPages nhỏ hơn 1 thì hidden
-                (totalPages > 1 &&
+                // (totalPages > 1 &&
                 <button
                   key={pageNumber}
                   onClick={() => handlePageClick(pageNumber - 1)}
@@ -58,7 +58,7 @@ export const Right = ({ filteredCourses, currentPage,handlePageClick, totalPages
                 >
                   {pageNumber}
                 </button>
-                )
+                // )
               )
             )}
           </div>
