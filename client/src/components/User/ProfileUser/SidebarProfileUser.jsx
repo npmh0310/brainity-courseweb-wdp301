a90a0b854bd898e2cd8815a4639cfa83b25e8319
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2024-07-23 11:13:10 +0700

    fix favorites, loading avatar, confirm course in teacher

--- client/src/components/User/ProfileUser/SidebarProfileUser.jsx
+++ client/src/components/User/ProfileUser/SidebarProfileUser.jsx
@@ -17,7 +17,7 @@ const SidebarProfileUser = () => {
   const [imagePreview, setImagePreview] = useState("");
   const fileInputRef = React.useRef(null);
   const [userData, setUserData] = useState({});
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const handleActiveItem = (item) => {
     setActiveItem(item);
   };
@@ -66,27 +66,28 @@ const SidebarProfileUser = () => {
     const formData = new FormData();
     formData.append("file", file);
     formData.append("upload_preset", preset_key);
-
+    setPendingImg(true);
     const res = await axios.post(
       "https://api.cloudinary.com/v1_1/doydh7dtj/image/upload",
       formData
     );
     if (res && res.status === 200) {
-
       const linkImg = {
         avatar: res.data.secure_url,
       };
-      const resUpdate = await updateAvatar(linkImg)
-     
+      const resUpdate = await updateAvatar(linkImg);
+
       if (resUpdate && resUpdate.status === 200) {
-        dispatch(validateToken())
+        dispatch(validateToken());
         toast.success("Updated success");
-       
+        setPendingImg(false);
       } else {
         toast.error("Something wrong");
+        setPendingImg(false);
       }
     } else {
       toast.error("Please select the image file");
+      setPendingImg(false);
     }
   };
 
@@ -95,6 +96,9 @@ const SidebarProfileUser = () => {
       fileInputRef.current.click();
     }
   };
+
+  // loading img
+  const [pendingImg, setPendingImg] = useState(false);
   return (
     <div className=" w-full md:w-[30%] max-h-[640px] bg-white mt-8 border pb-10 border-gray-200 rounded-md">
       <div className="pt-10 pb-8 border-b-[1px] ">
@@ -103,8 +107,11 @@ const SidebarProfileUser = () => {
         </h1>
         <div className="flex flex-col items-center gap-y-4 mt-8">
           <div className="relative">
+            {" "}
             <img
-              className="rounded-full w-[140px] h-[140px]"
+              className={`rounded-full w-[140px] h-[140px] ${
+                pendingImg && "opacity-20"
+              }`}
               src={user.avatar}
               alt=""
             />
@@ -120,6 +127,30 @@ const SidebarProfileUser = () => {
               className="absolute bottom-0 right-2 p-2 bg-gray-600 text-white rounded-full cursor-pointer"
               icon={faCamera}
             />
+            {pendingImg && (
+              <div
+                role="status"
+                className="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2"
+              >
+                <svg
+                  aria-hidden="true"
+                  className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span className="sr-only">Loading...</span>
+              </div>
+            )}
           </div>
           <h2 className="text-lg font-medium text-center">{user.username}</h2>
           <button className="border flex items-center gap-x-3  border-third px-5 py-3 rounded-md text-third text-[13px]">
