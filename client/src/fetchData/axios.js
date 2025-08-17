797d8ae89ded12264fd6d6da60a658c1f007c46a
Linh Nguyen <linhnq19@gmail.com>
2025-08-17 15:16:06 +0700

    last

--- client/src/fetchData/axios.js
+++ client/src/fetchData/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000/api/v1',
+    baseURL: `${process.env.REACT_APP_URL_CLIENT}/api/v1`,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
