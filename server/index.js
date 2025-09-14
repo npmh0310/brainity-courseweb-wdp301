0450f32e3b568434108ed679c95911a84d7359a4
tqqv <victortrnh@gmail.com>
2025-09-14 10:12:35 +0700

    Update cors

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 const corsOptions = {
-  origin: true,
+  origin: 'https://brainity-learnify.vercel.app',
   credentials: true,
 };
 
